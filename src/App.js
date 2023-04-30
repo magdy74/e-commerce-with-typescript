2cec00cd03ec3c9d9e5dc6c175b09e92de50e5ba
magdy74 <magdyraffet74@gmail.com>
2023-04-30 15:44:56 +0300

    created project with complete home page

--- src/App.js
+++ src/App.js
@@ -3,14 +3,30 @@ import './App.css';
 import HomePage from './pages/homepage/homepage-components';
 import { Route, Routes } from 'react-router';
 
-const HatsPage = () =>{
-  return(
-    
+const HatsPage = () =>(  
   <div>
     <h1>HatsPage</h1>
-  </div>
-  )
-}
+  </div>)
+
+const JacketsPage = () =>(  
+  <div>
+    <h1>JacketsPage</h1>
+  </div>)
+
+const ShoesPage = () =>(  
+  <div>
+    <h1>ShoesPage</h1>
+  </div>)
+
+const WomensPage = () =>(  
+  <div>
+    <h1>WomensPage</h1>
+  </div>)
+
+const MensPage = () =>(  
+  <div>
+    <h1>MensPage</h1>
+  </div>)
 
 
 
@@ -20,6 +36,10 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage/>}/>
         <Route path="/hats" element={<HatsPage/>}/>
+        <Route path="/jackets" element={<JacketsPage/>}/>
+        <Route path="/shoes" element={<ShoesPage/>}/>
+        <Route path="/womens" element={<WomensPage/>}/>
+        <Route path="/mens" element={<MensPage/>}/>
       </Routes>
     </div>
   )
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,18 +2,27 @@ import React from "react";
 import './header.css'
 import crownLogo from './4.3 crown.svg.svg'
 import cartLogo from './icons8-shopping-cart-30.png'
-const Header = () => (
-<div className="header">
-    <div className="rightPart">
-        <img className="img1"  alt= "img" src={crownLogo}/>
+import { useNavigate } from "react-router";
+const Header = () => {
+    const navigate = useNavigate()
+    return(
+    <div className="header">
+        <div className="rightPart">
+            <img className="imgCrown"  alt= "img" src={crownLogo}/>
+        </div>
+        <div className="leftPart">
+            <span onClick={()=> navigate('/collection')}>shop</span>
+            <span onClick={()=> navigate('/contacts')}>contact</span>
+            <span onClick={()=> navigate('/signin')}>sign in</span>
+            <img 
+                className="imgCart" 
+                alt= "img" 
+                src={cartLogo}
+                onClick={()=> navigate('/cart')}
+                />
+        </div>
     </div>
-    <div className="leftPart">
-        <span>shop</span>
-        <span>contact</span>
-        <span>sign in</span>
-        <img className="img2" alt= "img" src={cartLogo}/>
-    </div>
-</div>
-)
+    )
+}
 
 export default Header;

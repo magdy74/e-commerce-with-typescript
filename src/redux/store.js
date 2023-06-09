bfd730b3f5795bea749dd9c68a81084a42b0da82
magdy74 <magdyraffet74@gmail.com>
2023-06-09 15:02:54 +0300

    Created Project

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,11 @@ import logger from "redux-logger";
 import {persistStore} from 'redux-persist';
 import rootReducer from "./root-reducer";
 
-const middleWare = [logger];
+const middleWare = [];
+
+if (process.env.NODE_ENV === 'development'){
+    middleWare.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middleWare));
 
